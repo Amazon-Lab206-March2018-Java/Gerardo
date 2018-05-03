e13e8b61a27112b655d9df2cc229424c642177b4
Gerardo <jimgerar@amazon.com>
2018-05-03 17:29:10 -0600

    completed Human

--- springProjects/human/src/main/java/com/gerardo/human/controllers/IndexC.java
+++ springProjects/human/src/main/java/com/gerardo/human/controllers/IndexC.java
@@ -12,7 +12,7 @@ public class IndexC {
 	public String index( @RequestParam (value="name", defaultValue="Human") String query, Model model ) {
 		/*  ( HttpServletRequest request ) can be used to get the request
 			String name = request.getParameter("name"); then the request can be used to get the parameters (like a servlet)
-			and we can do model.addAttribute("name", name) to pass it to the jsp (just like below)
+			and we can do model.addAttribute("name", name) to pass it to the jsp (just like below).
 		*/
 		model.addAttribute("name", query);
 		
